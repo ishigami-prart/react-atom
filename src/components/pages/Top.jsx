5f86d3a78fde2200891551cb4178484de3ed9ee8
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-21 08:03:13 +0900

    途中から

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Top = () => {
+  return (
+    <SContainer>
+      <h2>TOPページです。</h2>
+    </Scontainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>User一覧</h2>
+    </Scontainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;
