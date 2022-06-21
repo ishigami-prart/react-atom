27e92a3aeb06c454e5f268636198679543752f1c
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-21 20:57:33 +0900

    41

--- src/components/atoms/pages/Users.jsx
+++ src/components/atoms/pages/Users.jsx
@@ -1,8 +1,11 @@
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { SearchInput } from "../molecules/Searchinput";
 import { UserCard } from "../../organisms/UserCard";
 import { useLocation } from "react-router-dom";
 import { SecondaryButton } from "../button/SecondaryButton";
+
+import { UserContext } from "../../../providers/UserProvider";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -29,12 +32,14 @@ const user = {
   website: "http://aaaa.jp"
 };
 export const Users = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const onClickSwitch = () => setUserInfo({});
   return (
     <SContainer>
       <h2>User一覧</h2>
       <SearchInput />
       <br />
-      <SecondaryButton>切り替え</SecondaryButton>
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
