3c5e22339078e38eaad95ffd08dcd6321331b270
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-22 17:30:18 +0900

    途中

--- src/components/atoms/molecules/user/UserIconWithName.jsx
+++ src/components/atoms/molecules/user/UserIconWithName.jsx
@@ -1,11 +1,14 @@
 import React, { useContext, memo } from "react";
 import styled from "styled-components";
-import { UserContext } from "../../../../providers/UserProvider";
+import { useRecoilValue } from "recoil";
+//import { UserContext } from "../../../../providers/UserProvider";
+import { uaserState } from "../../../../store/userState";
 
 export const UserIconWithName = memo((props) => {
   console.log("UserIconWithName");
   const { image, name } = props;
-  const { userInfo } = useContext(UserContext);
+  //const { userInfo } = useContext(UserContext);
+  const userInfo = useRecoilValue(userState);
   const isAdmin = userInfo ? userInfo.isAdmin : false;
 
   return (
