0f315902acd9b121de4b351b9b5f95b07f9fae0e
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-23 08:07:28 +0900

    finish

--- src/components/atoms/pages/Top.jsx
+++ src/components/atoms/pages/Top.jsx
@@ -3,10 +3,12 @@ import styled from "styled-components";
 import { SecondaryButton } from "../button/SecondaryButton";
 import { useHistory } from "react-router-dom";
 import { UserContext } from "../../../providers/UserProvider";
+import { useSetRecoilState } from "recoil";
+import { userState } from "../../../store/userState";
 
 export const Top = () => {
   const history = useHistory();
-  const { setUserInfo } = useContext(UserContext);
+  const setUserInfo = useSetRecoilState(userState);
   const onClickAdmin = () => {
     setUserInfo({ isAdmin: true });
     history.push("/users");
